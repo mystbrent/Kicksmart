292d1e26c6909874976db07393ca929323799bed
Brent Tudas <brent.tudas@gmail.com>
2017-04-16 09:29:39 +0800

    changed to seed shoes

--- src/frontend/app.js
+++ src/frontend/app.js
@@ -7,15 +7,9 @@ import authentication from 'feathers-authentication-client';
 const host = location.host
 const protocol = host.startsWith('local') ? 'http' : 'https';
 let url = `${protocol}://${location.host}`;
-console.log(url, ' da url');
 if(window.cordova) {
-    // url = 'http://169.254.48.107:3000';
-    url = 'http://192.168.1.100:8000';
-    alert(url)
+    // url = 'http://192.168.1.100:8000';
 }
-// url = 'http://192.168.1.3:3000';
-// const socketConnection = nativeSocketio(`${protocol}://${host}`);
-
 const socketConnection = nativeSocketio(url);
 const app = feathers()
     .configure(hooks())
